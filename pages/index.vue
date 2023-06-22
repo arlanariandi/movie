7a876a1fb75762e0d2148fc1b87e743f49d2e79c
arari3s <arlanariandi@gmail.com>
2023-06-22 11:37:12 +0700

    UPDATE: Add limit in movie card

--- pages/index.vue
+++ pages/index.vue
@@ -13,9 +13,9 @@
 
       <h2 class="text-2xl font-bold mb-4">Featured Movies</h2>
 
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
         <!-- Movie Card -->
-        <div v-for="movie in movies" :key="movie.id" class="bg-white shadow-md rounded-lg p-4">
+        <div v-for="movie in movies.slice(0, limit)" :key="movie.id" class="bg-white shadow-md rounded-lg p-4">
           <img :src="getImageUrl(movie.poster_path)" alt="Movie Poster" class="w-full rounded-lg mb-2">
           <h3 class="text-lg font-semibold mb-1">{{ movie.title }}</h3>
           <p class="text-gray-600">{{ movie.release_date }}</p>
@@ -31,7 +31,8 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      movies: []
+      movies: [],
+      limit: 10,
     };
   },
   mounted() {
@@ -42,7 +43,6 @@ export default {
     async fetchMovies() {
       try {
         const response = await axios.get(`${process.env.TMDB_API_URL}/movie/popular`, {
-          // this.movies = await this.$axios.get('/movie/popular', {
           params: {
             api_key: process.env.TMDB_API_KEY
           }
