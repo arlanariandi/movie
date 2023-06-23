5cad605d68935f1d921fc305125bd10e1376c911
arari3s <arlanariandi@gmail.com>
2023-06-23 21:48:09 +0700

    UPDATE: Add reating

--- pages/index.vue
+++ pages/index.vue
@@ -13,14 +13,21 @@
 
       <h2 class="text-2xl font-bold mb-4">Featured Movies</h2>
 
-      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
         <!-- Movie Card -->
         <div v-for="movie in movies.slice(0, limit)" :key="movie.id" class="bg-white shadow-md rounded-lg p-4">
-          <img :src="getImageUrl(movie.poster_path)" alt="Movie Poster" class="w-full rounded-lg mb-2">
+          <div class="relative overflow-hidden">
+            <img :src="getImageUrl(movie.poster_path)" alt="Movie Poster" class="w-full rounded-lg mb-2">
+            <p
+              class="flex absolute top-0 left-0 justify-center bg-orange-700 font-medium rounded-br-md w-10 h-10 items-center text-white">
+              {{ movie.vote_average }}
+            </p>
+          </div>
           <h3 class="text-lg font-semibold mb-1">{{ movie.title.slice(0, 20) }} <span
               v-if="movie.title.length > 20">...</span>
           </h3>
-          <p class="text-gray-600">Released:
+          <p class="text-gray-600">
+            <span class="font-medium">Released:</span>
             {{ new Date(movie.release_date).toLocaleString('en-us', {
               month: 'long',
               day: 'numeric',
@@ -41,7 +48,7 @@ export default {
   data() {
     return {
       movies: [],
-      limit: 12,
+      limit: 10,
     };
   },
   mounted() {
