55d5c920681179fff5bbe3cdd01f333f5abb34dd
arari3s <arlanariandi@gmail.com>
2023-06-29 23:13:43 +0700

    UPDATE: Update search movies

---
+++ components/MovieSearch.vue
@@ -0,0 +1,48 @@
+<template>
+  <!-- Image -->
+  <div class="flex w-full rounded-lg border border-gray-300">
+    <img
+      :src="getImageUrl(movie.poster_path)"
+      :alt="movie.title"
+      class="mr-4 h-36 w-24 rounded-l-lg object-cover"
+    />
+    <div class="mt-4">
+      <h2 class="text-lg font-bold">
+        {{ movie.title }}
+      </h2>
+      <!-- Released Date -->
+      <p class="text-gray-400">
+        {{
+          new Date(movie.release_date).toLocaleString('en-us', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
+          })
+        }}
+      </p>
+      <p class="mt-4 text-gray-600">
+        {{ movie.overview.slice(0, 350) }}
+        <span v-if="movie.overview.length > 350">...</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    getImageUrl(posterPath) {
+      if (!posterPath) {
+        return 'https://via.placeholder.com/300x450?text=No+Poster'
+      }
+      return `https://image.tmdb.org/t/p/w300${posterPath}`
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,40 @@
 <template>
   <main class="container mx-auto my-8 px-4 lg:px-0">
     <!-- Search movies -->
-    <form>
-      <label
-        for="default-search"
-        class="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      >
-        Search
-      </label>
-      <div class="relative">
-        <div
-          class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
-        >
-          <svg
-            aria-hidden="true"
-            class="h-5 w-5 text-gray-500 dark:text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
-        </div>
-        <input
-          type="search"
-          id="default-search"
-          class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
-          placeholder="Search movies..."
-          required
-        />
-        <button
-          type="submit"
-          class="absolute bottom-2.5 right-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
-        >
-          Search
-        </button>
-      </div>
-    </form>
-
-    <!-- <div class="mb-4 flex items-center justify-between">
+    <div class="mb-8 flex items-center justify-center">
       <input
+        v-model="query"
         type="text"
-        v-model="searchQuery"
         placeholder="Search movies..."
-        class="rounded-lg border border-gray-300 px-4 py-2 focus:border-blue-300 focus:outline-none focus:ring"
+        class="mr-2 w-1/2 rounded-lg border border-gray-300 p-2"
+        @keydown.enter="searchMovies"
       />
       <button
-        class="rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+        @click="searchMovies"
+        class="mr-2 rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-800"
       >
         Search
       </button>
       <button
         @click="resetSearch"
-        class="rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-600"
+        class="rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-red-800"
       >
         Reset
       </button>
+    </div>
+
+    <div v-if="loading" class="text-center">Loading...</div>
+    <!-- <div v-else-if="movies.length === 0" class="text-center">
+      No movies found.
     </div> -->
+    <div v-else>
+      <div class="grid gap-4">
+        <MovieSearch v-for="movie in movies" :key="movie.id" :movie="movie" />
+      </div>
+    </div>
+
     <!-- Popular Movies -->
-    <h2 class="mb-4 mt-6 text-2xl font-bold">Popular Movies</h2>
+    <h2 class="mb-4 mt-12 text-2xl font-bold">Popular Movies</h2>
     <div class="mb-10 flex flex-nowrap overflow-x-auto">
       <MovieCard
         v-for="movie in popularMovies"
@@ -89,9 +59,11 @@ export default {
   layout: 'dashboard',
   data() {
     return {
-      searchQuery: '',
+      query: '',
+      movies: [],
       popularMovies: [],
       topRatedMovies: [],
+      loading: false,
     }
   },
   async mounted() {
@@ -117,21 +89,30 @@ export default {
     }
   },
   methods: {
-    // Fetch popular movies
-    // async fetchPopular() {
-    //   try {
-    //     const response = await fetch(
-    //       `${process.env.TMDB_API_URL}/movie/popular?api_key=${process.env.TMDB_API_KEY}`
-    //     )
-    //     const data = await response.json()
-    //     this.popularMovies = data.results
-    //   } catch (error) {
-    //     console.error('Error fetching popular movies:', error)
-    //   }
-    // },
+    // Search Movies
+    searchMovies() {
+      this.loading = true
+      this.movies = []
+
+      this.$axios
+        .get('/search/movie', {
+          params: {
+            api_key: `${process.env.TMDB_API_KEY}`,
+            query: this.query,
+          },
+        })
+        .then((response) => {
+          this.loading = false
+          this.movies = response.data.results
+        })
+        .catch((error) => {
+          console.error('Error:', error)
+        })
+    },
+    // Button reset search movies
     resetSearch() {
-      this.searchQuery = ''
-      // this.fetchMovies()
+      this.query = ''
+      this.movies = []
     },
   },
 }
